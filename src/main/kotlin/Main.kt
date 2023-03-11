78a8b0bea8e84f120ce502639ceff0cd7b3a5b96
Shivam Pandey <shivam8kp@gmail.com>
2023-03-11 22:31:44 +0530

    add function practice

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,2 +1,27 @@
 fun main() {
+//    Take array of Integers then return max and min values.
+    val numbers = arrayOf(1, 3, 5, 6, 7);
+    val isMax = false;
+    val result = getMaxorMinValue(numbers, isMax)
+    println(result)
+}
+
+fun getMaxorMinValue(numbers: Array<Int>, isMax: Boolean): Int {
+    if (isMax) {
+        var max = 0;
+        for (number in numbers) {
+            if (number > max) {
+                max = number
+            }
+        }
+        return max
+    } else {
+        var min = 0;
+        for (number in numbers) {
+            if (number < min) {
+                min = number
+            }
+        }
+        return min
+    }
 }
